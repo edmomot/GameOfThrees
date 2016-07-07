9de9469b068744fc56106c87261508b5a06cc2ed
Ed Momot <emomot@hmbnet.com>
2016-07-07 16:38:54 -0400

    Shell for Game interface and service

---
+++ app/businessObjects/IGame.ts
@@ -0,0 +1,5 @@
+﻿export interface IGame
+{
+    start: number;
+    current?: number;
+}
---
+++ app/services/GameService.ts
@@ -0,0 +1,23 @@
+﻿import { Injectable } from '@angular/core';
+import { IGame } from '../businessObjects/IGame';
+
+@Injectable()
+export class GameService {
+    constructor() {
+        
+    }
+
+    start(n: number): IGame {
+        return {start: n};
+    }
+
+    divisibleByThree(): boolean {
+        return true;
+    }
+
+    subtractable(): boolean {
+        return true;
+    }
+
+
+}
