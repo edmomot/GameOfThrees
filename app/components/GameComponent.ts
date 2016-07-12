13e4bd1feac9edb83bf16e7433e7f3ecbddc0072
emomot <emomot@HMBX1YOG0466>
2016-07-12 10:47:03 -0400

    Moved game into its own component

---
+++ app/components/GameComponent.ts
@@ -0,0 +1,38 @@
+﻿import { Component } from '@angular/core';
+import { GameService } from '../services/GameService'
+import { IGame } from "../businessObjects/IGame";
+
+@Component({
+    selector: 'game',
+    templateUrl: 'build/components/GameComponent.html'
+})
+export class GameComponent {
+    divisible: boolean;
+    subtractable: boolean;
+    current: number;
+    start: number;
+
+    constructor(
+        private gameService: GameService) {
+    }
+
+    ngOnInit() : void {
+        this.subscribe();
+    }
+
+    subscribe() {
+        this.gameService.divisibilityEmitter.subscribe((data) => 
+            this.divisible = data
+        );
+
+        this.gameService.subtractableEmitter.subscribe((data) =>
+            this.subtractable = data
+        );
+
+        this.gameService.currentEmitter.subscribe((data) => 
+            this.current = data
+        );
+
+        this.gameService.init(200);
+    }
+}
--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,39 +1,12 @@
 import { Component, EventEmitter } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { GameService } from '../../services/GameService'
-import { IGame } from "../../businessObjects/IGame";
-import {Observable} from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
+import { GameComponent } from '../../components/GameComponent'
 
 @Component({
-    templateUrl: 'build/pages/page1/page1.html'
+    templateUrl: 'build/pages/page1/page1.html',
+    directives: [GameComponent]
 })
 export class Page1 {
-    divisible: boolean;
-    subtractable: boolean;
-    current: number;
 
-    constructor(
-        private navController: NavController,
-        private gameService: GameService) {
-    }
-
-    ngOnInit() : void {
-        this.subscribe();
-    }
-
-    subscribe() {
-        this.gameService.divisibilityEmitter.subscribe((data) => 
-            this.divisible = data
-        );
-
-        this.gameService.subtractableEmitter.subscribe((data) =>
-            this.subtractable = data
-        );
-
-        this.gameService.currentEmitter.subscribe((data) => 
-            this.current = data
-        );
-
-        this.gameService.init(200);
-    }
 }
